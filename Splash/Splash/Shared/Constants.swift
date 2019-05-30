4cefefddb5ffcf23d72db6d822f5147fcbafba7f
Sergiy Kostrykin <serg.kostrykin@gmail.com>
2019-05-30 13:53:18 +0300

    Pods added to repo

---
+++ Splash/Splash/Shared/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Splash
+//
+//  Created by Sergiy Kostrykin on 5/30/19.
+//  Copyright © 2019 SSK. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static let imagesPerRow: Int = 3
+    
+    static let rowsPerPage: Int = 10
+    
+    static let pagesMax: Int = 10
+    
+    static let minSearchQuery: Int = 3
+    
+    static let searchResultsPerRow: Int = 1
+    
+    
+    
+}
+
+
